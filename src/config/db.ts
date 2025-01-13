1ce81c9861752c07de31b235ac436043963236cc
Arsh Mittal <arshmittalsitl@gmail.com>
2025-01-13 21:48:24 +0530

    sql

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,13 +2,21 @@ import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
 dotenv.config();
+require('dotenv').config();
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: 3306      ,
 });
+db.getConnection()
+  .then(() => {
+    console.log('Connected to MySQL');
+  })
+  .catch((err) => {
+    console.error('MySQL connection error:', err);
+  });
+
 
 export default db;
